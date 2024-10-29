0faa8a71a084dad0e7bd470ccad7bc60159a17b2
KIDSUS\pllibre <pllibre@ext.kidsandus.com>
2024-10-29 16:40:13 +0100

    fix

--- app/[locale]/blog/holiday-activities/page.tsx
+++ app/[locale]/blog/holiday-activities/page.tsx
@@ -2,17 +2,21 @@ import { setRequestLocale } from 'next-intl/server';
 import HolidayActivitiesContent from './components/HolidayActivitiesContent';
 
 export const generateStaticParams = () => {
-  return [{ locale: 'en' }, { locale: 'es' }, { locale: 'cat' }];
+  return [
+    { locale: 'en' },
+    { locale: 'es' },
+    { locale: 'cat' },
+    { locale: 'fr' },
+    { locale: 'it' },
+  ];
 };
 
-export default async function HolidayActivities({ 
-  params 
-}: { 
-  params: { locale: string } 
-}) {
+interface PageProps {
+  params: { locale: string };
+}
+
+export default function HolidayActivities({ params }: PageProps) {
   setRequestLocale(params.locale);
 
-  return (
-    <HolidayActivitiesContent />
-  );
+  return <HolidayActivitiesContent />;
 }
